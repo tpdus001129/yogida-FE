ac3f70e7b605b97a1e64b6cbb049f33e10871878
Dahyeon Lee <dada4202@gmail.com>
2023-12-18 14:33:07 +0900

    feat: #7 마이페이지 내 글 작성 UI component 구현 중

--- src/components/Mypage/Schedule.jsx
+++ src/components/Mypage/Schedule.jsx
@@ -10,8 +10,10 @@ export default function Schedule() {
       <section className="w-full top-0 border-[#E8E8E8] border-b-[1px]">
         <header className="w-full h-header bg-white text-center flex items-center">
           <IoChevronBack size={32} className="pl-2 cursor-pointer justify-self-start" onClick={() => navigate(-1)} />
-          <h1 className="text-[18px] font-bold">여기다 글쓰기</h1>
-          <img src={logo} alt="logo" />
+          <div className="border w-full flex items-center gap-[8px]">
+            <h1 className="text-[18px] font-bold">여기다 글쓰기</h1>
+            <img src={logo} alt="logo" />
+          </div>
         </header>
       </section>
     </>
