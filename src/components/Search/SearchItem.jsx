8011bfd3a7cdde33c4fce152387e54ca6876e8ea
hayoona <luxureny@gmail.com>
2023-12-21 00:29:41 +0900

    feat: #29 최근 검색어 기능 구현

--- src/components/Search/SearchItem.jsx
+++ src/components/Search/SearchItem.jsx
@@ -1,17 +1,23 @@
+import PropTypes from 'prop-types';
+
 import { IoClose } from 'react-icons/io5';
 
-export default function SearchItem() {
+export default function SearchItem({ keyword, onRemove }) {
   return (
     <div>
       <div className="relative">
         <div className="w-full h-[52px] flex justify-between items-center px-[24px]">
-          <p>keyword</p>
-          <button>
+          <p>{keyword}</p>
+          <button onClick={onRemove}>
             <IoClose />
           </button>
         </div>
-        <hr className="w-full absolute bottom-0 border-gray-1" />
       </div>
     </div>
   );
 }
+
+SearchItem.propTypes = {
+  keyword: PropTypes.string.isRequired,
+  onRemove: PropTypes.func.isRequired,
+};
