7646abbf277c3384f0970c30285226b937b3d2ed
Dahyeon Lee <dada4202@gmail.com>
2023-12-20 18:08:43 +0900

    feat:#26 navbar 디자인 변경

--- src/components/commons/Navbar.jsx
+++ src/components/commons/Navbar.jsx
@@ -1,23 +1,31 @@
 import { NavLink } from 'react-router-dom';
-import { IoSearchOutline } from 'react-icons/io5';
-import { IoAddCircleOutline } from 'react-icons/io5';
-import { IoPersonOutline } from 'react-icons/io5';
+import { IoHomeOutline } from 'react-icons/io5';
+// import { IoHomeSharp } from 'react-icons/io5';
+import { IoNotificationsOutline } from 'react-icons/io5';
+// import { IoNotificationsSharp } from 'react-icons/io5';
+import { IoPersonCircleOutline } from 'react-icons/io5';
+import { PiNotePencil } from 'react-icons/pi';
 
 const TABS = [
   {
-    name: '둘러보기',
-    icon: <IoSearchOutline size={26} />,
+    title: '홈',
+    icon: <IoHomeOutline size={26} />,
     path: '/',
   },
   {
-    name: '글작성',
-    icon: <IoAddCircleOutline size={26} />,
-    path: '/add',
+    title: '알림',
+    icon: <IoNotificationsOutline size={26} />,
+    path: '/notification',
   },
   {
-    name: '마이페이지',
-    icon: <IoPersonOutline size={26} />,
-    path: '/mypage',
+    title: '일정 작성',
+    icon: <PiNotePencil size={26} />,
+    path: '/schedule',
+  },
+  {
+    title: '로그인',
+    icon: <IoPersonCircleOutline size={26} />,
+    path: '/login',
   },
 ];
 
@@ -26,13 +34,12 @@ export default function Navbar() {
     <nav className="border-t-[1px] border-solid border-gray w-full fixed bottom-0 left-0 h-navbar bg-white z-10">
       <ul className="flex items-center h-full w-full px-[20px] py-[12px]">
         {TABS.map((tab) => (
-          <li key={tab.name} className="flex-1 h-full">
+          <li key={tab.title} className="flex-1 h-full">
             <NavLink
               to={tab.path}
-              className="flex flex-col items-center w-fit m-auto h-full text-black aria-[current=page]:text-primary"
+              className="flex items-center w-fit m-auto h-full text-black aria-[current=page]:text-primary"
             >
               {tab.icon}
-              <span className="text-[12px] ">{tab.name}</span>
             </NavLink>
           </li>
         ))}
