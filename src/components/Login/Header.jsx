fa98f2a89513c8ff2b99661e0907145ac9d4725b
Dahyeon Lee <dada4202@gmail.com>
2023-12-26 02:39:50 +0900

    fix: 로그인 페이지에서 뒤로 이동 시 메인 페이지로 이동하게 수정

--- src/components/Login/Header.jsx
+++ src/components/Login/Header.jsx
@@ -1,13 +1,11 @@
 import { IoChevronBack } from 'react-icons/io5';
-import { useNavigate } from 'react-router-dom';
-
+import { PATH } from '../../constants/path';
 import PropTypes from 'prop-types';
 
 export default function Header({ place, category }) {
-  const navigate = useNavigate();
   return (
     <header className="w-full h-header bg-white text-center fixed top-0 left-0 flex items-center">
-      <IoChevronBack size={'32px'} className="pl-2 cursor-pointer" onClick={() => navigate(-1)} />
+      <IoChevronBack size={'32px'} className="pl-2 cursor-pointer" onClick={() => (window.location.href = PATH.root)} />
       {place && (
         <div className="w-full h-full flex justify-center items-center gap-[4px]">
           <p className="text-[14px] font-bold">{place}</p>
