6bbdf295929e740d9f6ab51e0a4569d6c93d875b
Dahyeon Lee <dada4202@gmail.com>
2023-12-30 07:51:13 +0900

    node delete

--- node_modules/react-kakao-maps-sdk/dist/hooks/useIsomorphicLayoutEffect.cjs
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var React = require('react');
-
-const useIsomorphicLayoutEffect = typeof window !== "undefined" && typeof document !== "undefined" ? React.useLayoutEffect : React.useEffect;
-
-exports.useIsomorphicLayoutEffect = useIsomorphicLayoutEffect;
--- node_modules/react-kakao-maps-sdk/dist/util/constants.cjs
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const SIGNATURE = `__react-kakao-maps-sdk__`;
-
-exports.SIGNATURE = SIGNATURE;
